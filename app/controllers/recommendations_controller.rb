22bf0938464f38a1c4aeeb6d7671d636df32afa5
Jeff Cohen <jeff@starterleague.com>
2013-10-21 13:02:11 -0500

    Use a local variable to generate the query string values; and DRY up the controller code.

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,43 +1,33 @@
 class RecommendationsController < ApplicationController
 
+  def index
+    @recommendations = all_recommendations
+  end
+
   def details
     the_recommendation_id = params["id"].to_i
-    recommendations = [
-      {
-        id: 0,
-        title: "Apollo 13",
-        description: "Documentary about how to read error messages."
-      },
-      {
-        id: 1,
-        title: "Adler Planetarium",
-        description: "See the skyshow"
-      },
-      {
-        id: 2,
-        title: 'The PickAxe',
-        description: "The book every Ruby dev should own."}
-    ]
 
-    @item = recommendations[the_recommendation_id]
+    @item = all_recommendations[the_recommendation_id]
   end
 
-  def index
-    @recommendations = [
-      {
-        id: 0,
-        title: "Apollo 13",
-        description: "Documentary about how to read error messages."
-      },
-      {
-        id: 1,
-        title: "Adler Planetarium",
-        description: "See the skyshow"
-      },
-      {
-        id: 2,
-        title: 'The PickAxe',
-        description: "The book every Ruby dev should own."}
-    ]
+
+  def all_recommendations
+    return [{
+                id: 0,
+                title: "Apollo 13",
+                description: "Documentary about how to read error messages."
+              },
+              {
+                id: 1,
+                title: "Adler Planetarium",
+                description: "See the skyshow"
+              },
+              {
+                id: 2,
+                title: 'The PickAxe',
+                description: "The book every Ruby dev should own."
+              }
+            ]
   end
+
 end
