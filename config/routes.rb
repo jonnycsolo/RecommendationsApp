0ec4e1c18492d65ed4da0697164f0ece38223c03
Jeff Cohen <jeff@starterleague.com>
2013-10-23 10:17:11 -0500

    Use named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 RecommendationsApp::Application.routes.draw do
 
+  root 'recommendations#index'
+
   # Display all item
   get "/all" => 'recommendations#index'
 
   # Show the details of just one item
   # http://localhost:3000/details/98
   # params = { "id" => "98"}
-  get "/details/:id" => 'recommendations#show'
+  get "/details/:id" => 'recommendations#show', :as => "details"
 
 end
