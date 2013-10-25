12b20f6ee1c655de3e267dc3d003631d52eec78b
Jeff Cohen <jeff@starterleague.com>
2013-10-25 10:06:55 -0500

    Delete items and make everything database-backed

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,14 +1,17 @@
 class RecommendationsController < ApplicationController
 
+  def destroy
+    item = Item.find_by(id: params["id"].to_i)
+    item.destroy
+    redirect_to root_url
+  end
+
   def index
     @recommendations = Item.all
   end
 
   def show
-    the_recommendation_id = params["id"].to_i
-
-    @item = all_recommendations[the_recommendation_id]
-
+    @item = Item.find_by(id: params["id"].to_i)
     render 'details'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ RecommendationsApp::Application.routes.draw do
   # http://localhost:3000/details/98
   # params = { "id" => "98"}
   get "/details/:id" => 'recommendations#show', :as => "details"
-
+  get "/delete/:id" => 'recommendations#destroy'
 end
