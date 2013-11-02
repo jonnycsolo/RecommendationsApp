aa83c41315786d34f3d117573ad3c62175a23ae5
Jeff Cohen <jeff@starterleague.com>
2013-11-02 12:34:49 -0500

    Use resources line instead of handwriting all seven RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 RecommendationsApp::Application.routes.draw do
 
-  # root 'recommendations#index'
+  root 'recommendations#index'
 
   get "/about" => 'pages#about'
 
-  get "/recommendations" => 'recommendations#index'
+  resources :recommendations
 
-  get "/recommendations/new" => 'recommendations#new', :as => 'new_recommendation'
-  post "/recommendations" => 'recommendations#create'
+  # get "/recommendations" => 'recommendations#index'
 
-  get "/recommendations/:id" => 'recommendations#show', :as => "recommendation"
-  delete "/recommendations/:id" => 'recommendations#destroy'
+  # get "/recommendations/new" => 'recommendations#new', :as => 'new_recommendation'
+  # post "/recommendations" => 'recommendations#create'
 
-  get "/recommendations/:id/edit" => 'recommendations#edit', :as => 'edit_recommendation'
-  patch "/recommendations/:id" => 'recommendations#update'
+  # get "/recommendations/:id" => 'recommendations#show', :as => "recommendation"
+  # delete "/recommendations/:id" => 'recommendations#destroy'
+
+  # get "/recommendations/:id/edit" => 'recommendations#edit', :as => 'edit_recommendation'
+  # patch "/recommendations/:id" => 'recommendations#update'
 end
