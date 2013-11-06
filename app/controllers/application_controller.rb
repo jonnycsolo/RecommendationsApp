048f7d960fe7b7a86a7cef425df56ed0e85a825d
jonnycsolo <Jonathan@Jonathans-MacBook-Pro.local>
2013-11-06 15:37:23 -0600

    still playing around with authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,3 +5,10 @@ class ApplicationController < ActionController::Base
 before_filter :authenticate
 
 
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['HTTP_USERNAME'] && password == ENV['HTTP_PASSWORD']
+    end
+  end
+end
+
