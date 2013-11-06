e21885e591722658b0858f9446f4353ee207bef0
jonnycsolo <Jonathan@Jonathans-MacBook-Pro.local>
2013-11-06 15:35:00 -0600

    hmm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 before_filter :authenticate
 
-  def authenticate
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['HTTP_USERNAME'] && password == ENV['HTTP_PASSWORD']
-    end
-  end
-end
 
